0e547758bf3a5562944a8cab66f87b7852eaa818
Susan <susanxmy@gmail.com>
2017-11-24 17:05:58 +0100

    Initial Commit

---
+++ WS1718SiriKitTests/WS1718SiriKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  WS1718SiriKitTests.swift
+//  WS1718SiriKitTests
+//
+//  Created by Susan Xue on 24/11/2017.
+//  Copyright © 2017 RWTH Aachen University (Media Computing Group). All rights reserved.
+//
+
+import XCTest
+@testable import WS1718SiriKit
+
+class WS1718SiriKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
